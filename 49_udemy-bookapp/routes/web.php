40681c427890e19a988016ced57916eb1f04a837
aocattleya <aocattleya@yahoo.co.jp>
2020-05-24 19:52:12 +0900

    add: 削除機能の追加

--- 49_udemy-bookapp/routes/web.php
+++ 49_udemy-bookapp/routes/web.php
@@ -38,3 +38,8 @@ Route::post('/book', function(Request $request) {
 
     return redirect('/');
 });
+
+Route::delete('/book/{book}', function(Book $book){
+    $book->delete();
+    return redirect('/');
+});
